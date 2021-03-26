39dd4d8261dc871570786a8c95fa53dda8b14e24
1garo <alevardai@hotmail.com>
2021-03-26 19:10:42 -0300

    feat: add parsing func to handler

--- handler/handler.go
+++ handler/handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/PicPay/software-engineer-challenge/db"
+	"github.com/PicPay/software-engineer-challenge/models"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 )
@@ -29,4 +31,26 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(400)
 	render.Render(w, r, ErrNotFound)
+}
+
+func Parsing(resp *chan []string, res, resOrdered, resAux *models.UserList) {
+	ids_1 := map[string]int{}
+	aux, i := 0, 0
+	for r := range *resp {
+		ids_1[r[0]] = aux
+		aux += 1
+	}
+
+	sort.Slice(
+		res.Users,
+		func(i, j int) bool { return ids_1[res.Users[i].ID] < ids_1[res.Users[j].ID] })
+
+	for _, user := range res.Users {
+		if _, ok := ids_1[user.ID]; ok {
+			resOrdered.Users = append(resOrdered.Users, user)
+			i += 1
+		} else {
+			resAux.Users = append(resAux.Users, user)
+		}
+	}
 }
